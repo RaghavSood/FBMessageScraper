af20a3a86e36619a4556e6fc1138c42a9d7c04a7
Raghav Sood <raghavsood@gmail.com>
2014-09-29 21:22:42 +0530

    Added initial script

---
+++ README.md
@@ -0,0 +1,35 @@
+Facebook Message Scraper
+========================
+
+A simple python script to download the entire conversation from Facebook, not limited like the one in the data dump provided by Facebook
+
+Outputs the conversation in a JSON format, as well as the JSON for each individual chunk.
+
+Initial Setup
+=============
+
+1. In Chrome, open [facebook.com/messages](https://www.facebook.com/messages/) and open any conversation with a fair number of messages
+2. Open the network tab of the Chrome Developer tools
+3. Scroll up in the conversation until the page attempts to load previous messages
+4. Look for the POST request to [thread\_info.php](https://www.facebook.com/ajax/mercury/thread_info.php)
+5. You need to copy certain parameters from this request into the python script to complete the setup:
+  1. Set the `cookie` value to the value you see in Chrome under `Request Headers`
+  2. Set the `__user` value to the value you see in Chrome under `Form Data` 
+  3. Set the `__a` value to the value you see in Chrome under `Form Data`
+  4. Set the `__dyn` value to the value you see in Chrome under `Form Data`
+  5. Set the `__req` value to the value you see in Chrome under `Form Data`
+  6. Set the `fb_dtsg` value to the value you see in Chrome under `Form Data`
+  7. Set the `ttstamp` value to the value you see in Chrome under `Form Data`
+  8. Set the `__rev` value to the value you see in Chrome under `Form Data`
+
+You're now all set to start downloading messages.
+
+Downloading Messages
+====================
+
+1. Get the conversation ID for those messages by opening [http://graph.facebook.com/{username-of-chat-partner}](http://graph.facebook.com/{username_of_chat_partner})
+2. Copy the `id` value from there
+3. For group conversations, the ID can be retrieved from the messages tab, as part of the URL
+4. Run the command `python dumper.py {id} 2000`, and put the value you retrieved for ID earlier
+
+Messages are saved by default to `~/Development/Facebook/MessagesDump/Messages/{id}/`
---
+++ dumper.py
@@ -0,0 +1,106 @@
+import urllib2
+import urllib
+import gzip
+import os
+import json
+import sys
+import time
+import StringIO
+
+__author__ = "Raghav Sood"
+__copyright__ = "Copyright 2014"
+__credits__ = ["Raghav Sood"]
+__license__ = "CC"
+__version__ = "1.0"
+__maintainer__ = "Raghav Sood"
+__email__ = "raghavsood@appaholics.in"
+__status__ = "Production"
+
+if len(sys.argv) <= 1:
+	print "Usage:\n 	python dumper.py [conversation ID] [chunk_size (recommended: 2000)] [{optional} offset location (default: 0)]"
+	print "Example conversation with Raghav Sood"
+	print "	python dumper.py 1075686392 2000 0"
+	sys.exit()
+
+error_timeout = 30 # Change this to alter error timeout (seconds)
+general_timeout = 7 # Change this to alter waiting time afetr every request (seconds)
+messages = []
+talk = sys.argv[1]
+offset = int(sys.argv[3]) if len(sys.argv) >= 4 else int("0")
+messages_data = "lolno"
+end_mark = "\"payload\":{\"end_of_history\""
+limit = int(sys.argv[2])
+headers = {"origin": "https://www.facebook.com", 
+"accept-encoding": "gzip,deflate", 
+"accept-language": "en-US,en;q=0.8", 
+"cookie": "your_cookie_value", 
+"pragma": "no-cache", 
+"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.122 Safari/537.36", 
+"content-type": "application/x-www-form-urlencoded", 
+"accept": "*/*", 
+"cache-control": "no-cache", 
+"referer": "https://www.facebook.com/messages/zuck"}
+
+base_directory = "~/Development/Facebook/MessagesDump/Messages/"
+directory = base_directory + str(talk) + "/"
+pretty_directory = base_directory + str(talk) + "/Pretty/"
+
+try:
+	os.makedirs(directory)
+except OSError:
+	pass # already exists
+
+try:
+	os.makedirs(pretty_directory)
+except OSError:
+	pass # already exists
+
+while end_mark not in messages_data:
+
+	data_text = {"messages[user_ids][" + str(talk) + "][offset]": str(offset), 
+	"messages[user_ids][" + str(talk) + "][limit]": str(limit), 
+	"client": "web_messenger", 
+	"__user": "your_user_id", 
+	"__a": "your __a", 
+	"__dyn": "your __dyn", 
+	"__req": "your __req", 
+	"fb_dtsg": "your_fb_dtsg", 
+	"ttstamp": "your_ttstamp", 
+	"__rev": "your __rev"}
+	data = urllib.urlencode(data_text)
+	url = "https://www.facebook.com/ajax/mercury/thread_info.php"
+	
+	print "Retrieving messages " + str(offset) + "-" + str(limit+offset) + " for conversation ID " + str(talk)
+	req = urllib2.Request(url, data, headers)
+	response = urllib2.urlopen(req)
+	compressed = StringIO.StringIO(response.read())
+	decompressedFile = gzip.GzipFile(fileobj=compressed)
+	
+	
+	outfile = open(directory + str(offset) + "-" + str(limit+offset) + ".json", 'w')
+	messages_data = decompressedFile.read()
+	messages_data = messages_data[9:]
+	json_data = json.loads(messages_data)
+	if json_data is not None and json_data['payload'] is not None:
+		try:
+			messages = messages + json_data['payload']['actions']
+		except KeyError:
+			pass #no more messages
+	else:
+		print "Error in retrieval. Retrying after " + str(error_timeout)"s"
+		print "Data Dump:"
+		print json_data
+		time.sleep(error_timeout)
+		continue
+	outfile.write(messages_data)
+	outfile.close()	
+	command = "python -mjson.tool " + directory + str(offset) + "-" + str(limit+offset) + ".json > " + pretty_directory + str(offset) + "-" + str(limit+offset) + ".pretty.json"
+	os.system(command)
+	offset = offset + limit
+	time.sleep(general_timeout) 
+
+finalfile = open(directory + "complete.json", 'wb')
+finalfile.write(json.dumps(messages))
+finalfile.close()
+command = "python -mjson.tool " + directory + "complete.json > " + pretty_directory + "complete.pretty.json"
+os.system(command)
